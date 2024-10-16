78c815f3644d6e78b5260ea4bd9431a0590406f7
Propstg206~ <alexpropstg@gmail.com>
2024-10-16 23:04:14 +0200

    better js structure

---
+++ scripts/login.js
@@ -0,0 +1,29 @@
+document.getElementById('loginForm').addEventListener('submit', async function(event) {
+    event.preventDefault(); // Evitar el envío por defecto del formulario
+
+    const username = document.getElementById('username').value; // Obtener el username
+    const password = document.getElementById('user_password').value;
+
+    try {
+        const response = await fetch('http://localhost:3000/api/login', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ username, password }),
+        });
+
+        const data = await response.json();
+
+        if (response.ok) {
+            // Si el login fue exitoso, redirigir a index.html
+            window.location.href = 'index.html';
+        } else {
+            // Manejo de errores, mostrar mensaje
+            alert(data.message || 'Login failed');
+        }
+    } catch (error) {
+        console.error('Error:', error);
+        alert('Error al intentar iniciar sesión');
+    }
+});
---
+++ scripts/main.js
@@ -0,0 +1,19 @@
+const toggleButton = document.getElementById('toggleTheme');
+toggleButton.addEventListener('click', function() {
+    const currentTheme = document.body.getAttribute('data-bs-theme');
+    if (currentTheme === 'dark') {
+        document.body.setAttribute('data-bs-theme', 'light');
+        toggleButton.textContent = 'Modo oscuro';
+    } else {
+        document.body.setAttribute('data-bs-theme', 'dark');
+        toggleButton.textContent = 'Modo claro';
+    }
+});
+// Mover la página a la derecha cuando se abra el menú
+const offcanvasMenu = document.getElementById('offcanvasMenu');
+offcanvasMenu.addEventListener('show.bs.offcanvas', function () {
+    document.body.classList.add('offcanvas-open');
+});
+offcanvasMenu.addEventListener('hidden.bs.offcanvas', function () {
+    document.body.classList.remove('offcanvas-open');
+});
